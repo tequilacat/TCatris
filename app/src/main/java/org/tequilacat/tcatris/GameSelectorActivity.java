9e9ed1dc7f9483c74eb2f1f4702b8308ec4b9dd1
avo <alexey.vdovichenko@opencascade.com>
2016-02-26 13:19:34 +0300

    more

--- app/src/main/java/org/tequilacat/tcatris/GameSelectorActivity.java
+++ app/src/main/java/org/tequilacat/tcatris/GameSelectorActivity.java
@@ -2,9 +2,9 @@ package org.tequilacat.tcatris;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.widget.ListView;
 
 import org.tequilacat.tcatris.core.GameList;
-import org.tequilacat.tcatris.core.TetrisCanvas;
 
 public class GameSelectorActivity extends Activity {
 
@@ -12,9 +12,12 @@ public class GameSelectorActivity extends Activity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     GameList gameList = new GameList();
+    ListView gameListView = (ListView) findViewById(R.id.lvMainOptionList);
+
+    //
     //TetrisCanvas gameView = new TetrisCanvas(this, gameList);
     //setContentView(gameView);
-    setContentView(R.layout.activity_blank);
+    setContentView(R.layout.activity_gameselector);
   }
 
 }
